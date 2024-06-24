8e40f64c703deb505a18fc5e1b35db33761646fa
Juan Frauca <juanfrauca@outlook.com>
2024-06-24 09:06:35 +0200

    Creacion Pantalla Login

--- RnpConnect-Bootstrap/src/app/app.routes.ts
+++ RnpConnect-Bootstrap/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import path from 'node:path';
+import { LoginComponent } from './paginas/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    //Ruta por defecto login
+    {
+        path: '',
+        component: LoginComponent,
+        pathMatch: 'full'
+        
+    }
+
+
+
+];
